ee0660fab7b8fea8a607c727e543c6c92563c988
txrus <mitch.xuixide@gmail.com>
2022-07-19 20:35:03 +0700

    update

--- guess_my_number/starter/script.js
+++ guess_my_number/starter/script.js
@@ -1,13 +1,10 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   //when there is no input
   if (!guess) {
@@ -58,9 +55,13 @@ function refreshPage() {
 }
 
 document.querySelector('.again').addEventListener('click', function() {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.highscore').textContent = highscore;
   document.querySelector('body').style.backgroundColor = "#222";
   document.querySelector('.score').textContent = '20';
+  document.querySelector('.message').textContent = 'Start guessing . . .';
+
 });
